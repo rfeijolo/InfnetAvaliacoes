7ed9c264f78f78f6a730fadf993f7a271a5e38ba
rodrigo.rodrigues <rodrigo.rodrigues@gastecnologia.com>
2014-08-07 23:18:46 -0300

    Rename de pastas e adicionando scaffold de curso

---
+++ src/Avaliacoes.Web/Areas/Admin/Controllers/CursosController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Avaliacoes.Data;
+using Avaliacoes.Domain;
+
+namespace Avaliacoes.Web.Areas.Admin.Controllers
+{
+    public class CursosController : Controller
+    {
+        private AvaliacoesDbContext db = new AvaliacoesDbContext();
+
+        // GET: Admin/Cursos
+        public ActionResult Index()
+        {
+            return View(db.Cursos.ToList());
+        }
+
+        // GET: Admin/Cursos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Curso curso = db.Cursos.Find(id);
+            if (curso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(curso);
+        }
+
+        // GET: Admin/Cursos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Cursos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome")] Curso curso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cursos.Add(curso);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(curso);
+        }
+
+        // GET: Admin/Cursos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Curso curso = db.Cursos.Find(id);
+            if (curso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(curso);
+        }
+
+        // POST: Admin/Cursos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome")] Curso curso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(curso).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(curso);
+        }
+
+        // GET: Admin/Cursos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Curso curso = db.Cursos.Find(id);
+            if (curso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(curso);
+        }
+
+        // POST: Admin/Cursos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Curso curso = db.Cursos.Find(id);
+            db.Cursos.Remove(curso);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
