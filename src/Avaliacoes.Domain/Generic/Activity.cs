f39e07bfecf8ee33cad14c11cc37c0bec2d8ea68
Felipe Barbirato <fbarbirato@gmail.com>
2014-08-11 23:04:10 -0300

    Acrescentadas classes de atividade e comando

---
+++ src/Avaliacoes.Domain/Generic/Activity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avaliacoes.Domain.Generic
+{
+    public class Activity
+    {
+        public Activity(Command command)
+        {
+            _command = command;
+        }
+
+        private Command _command;
+
+        public Message Initiate()
+        {
+            var message = _command.Validate();
+
+            if (message.CurrentStatus == Message.Status.Success)
+            {
+                _command.Execute(this);
+            }
+
+            return message;
+        }
+
+    }
+}
---
+++ src/Avaliacoes.Domain/Generic/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Avaliacoes.Domain.Generic
+{
+    public class Message
+    {
+        public enum Status
+        {
+            Success,
+            Failure
+        }
+
+        public Status CurrentStatus { get; set; }
+
+        public String CurrentMessage { get; set; }
+
+        public Message(String message, Status status)
+        {
+            this.CurrentMessage = message;
+            this.CurrentStatus = status;
+        }
+    }
+}
