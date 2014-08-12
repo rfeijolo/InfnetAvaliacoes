288239f1e6df95494bcc77350d6ed8e5b86e02c2
rodrigo.rodrigues <rodrigo.rodrigues@gastecnologia.com>
2014-08-11 23:39:40 -0300

    Ultimos scaffolds, refatorar e ver quais serão necessários. Integrar alunos e login

---
+++ src/Avaliacoes.Web/Areas/Admin/Controllers/TurmasController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Avaliacoes.Data;
+using Avaliacoes.Domain;
+
+namespace Avaliacoes.Web.Areas.Admin.Controllers
+{
+    public class TurmasController : Controller
+    {
+        private AvaliacoesDbContext db = new AvaliacoesDbContext();
+
+        // GET: Admin/Turmas
+        public ActionResult Index()
+        {
+            return View(db.Turmas.ToList());
+        }
+
+        // GET: Admin/Turmas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Turma turma = db.Turmas.Find(id);
+            if (turma == null)
+            {
+                return HttpNotFound();
+            }
+            return View(turma);
+        }
+
+        // GET: Admin/Turmas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Turmas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Codigo")] Turma turma)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Turmas.Add(turma);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(turma);
+        }
+
+        // GET: Admin/Turmas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Turma turma = db.Turmas.Find(id);
+            if (turma == null)
+            {
+                return HttpNotFound();
+            }
+            return View(turma);
+        }
+
+        // POST: Admin/Turmas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Codigo")] Turma turma)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(turma).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(turma);
+        }
+
+        // GET: Admin/Turmas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Turma turma = db.Turmas.Find(id);
+            if (turma == null)
+            {
+                return HttpNotFound();
+            }
+            return View(turma);
+        }
+
+        // POST: Admin/Turmas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Turma turma = db.Turmas.Find(id);
+            db.Turmas.Remove(turma);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
