79f7c313b7b86dfc68342f16421051c3c4e96624
rodrigo.rodrigues <rodrigo.rodrigues@gastecnologia.com>
2014-09-12 20:53:05 -0300

    Responder avaliacao

--- src/Avaliacoes.Domain/Resposta.cs
+++ src/Avaliacoes.Domain/Resposta.cs
@@ -1,12 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Avaliacoes.Domain
 {
     public class Resposta
     {
         public int Id { get; set; }
-        public Questao Questao { get; set; }
+        public int QuestaoId { get; set; }
+        
         [Range(1, 5)]
-        public int OpcaoEscolhida { get; set; }
+        public Likert OpcaoEscolhida { get; set; }
+
+        public string AlunoId { get; set; }
+        [ForeignKey("AlunoId")]
         public virtual Aluno Aluno { get; set; }
+        [ForeignKey("QuestaoId")]
+        public Questao Questao { get; set; }
     }
 }
