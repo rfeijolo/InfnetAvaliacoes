37400fc004e637f8be9ea834407c5b10a3dfc071
Felipe <fbarbirato@gmail.com>
2014-09-13 12:11:09 -0300

    View de respostas

---
+++ src/Avaliacoes.Web/Areas/Admin/Controllers/RespostasController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Avaliacoes.Domain;
+using Avaliacoes.Data;
+
+namespace Avaliacoes.Web.Areas.Admin.Controllers
+{
+    public class RespostasController : Controller
+    {
+        private AvaliacoesDbContext db = new AvaliacoesDbContext();
+
+        // GET: /Admin/Respostas/
+        public ActionResult Index()
+        {
+            var respostas = db.Respostas.Include(r => r.Aluno).Include(r => r.Avaliacao).Include(r => r.Questao);
+            return View(respostas.ToList());
+        }
+
+        // GET: /Admin/Respostas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resposta resposta = db.Respostas.Find(id);
+            if (resposta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(resposta);
+        }
+
+        // GET: /Admin/Respostas/Create
+        public ActionResult Create()
+        {
+            ViewBag.AlunoId = new SelectList(db.Users, "Id", "Name");
+            ViewBag.AvaliacaoId = new SelectList(db.Avaliacoes, "Id", "Objetivo");
+            ViewBag.QuestaoId = new SelectList(db.Questoes, "Id", "Texto");
+            return View();
+        }
+
+        // POST: /Admin/Respostas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,QuestaoId,OpcaoEscolhida,AlunoId,AvaliacaoId")] Resposta resposta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Respostas.Add(resposta);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AlunoId = new SelectList(db.Users, "Id", "Name", resposta.AlunoId);
+            ViewBag.AvaliacaoId = new SelectList(db.Avaliacoes, "Id", "Objetivo", resposta.AvaliacaoId);
+            ViewBag.QuestaoId = new SelectList(db.Questoes, "Id", "Texto", resposta.QuestaoId);
+            return View(resposta);
+        }
+
+        // GET: /Admin/Respostas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resposta resposta = db.Respostas.Find(id);
+            if (resposta == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AlunoId = new SelectList(db.Users, "Id", "Name", resposta.AlunoId);
+            ViewBag.AvaliacaoId = new SelectList(db.Avaliacoes, "Id", "Objetivo", resposta.AvaliacaoId);
+            ViewBag.QuestaoId = new SelectList(db.Questoes, "Id", "Texto", resposta.QuestaoId);
+            return View(resposta);
+        }
+
+        // POST: /Admin/Respostas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,QuestaoId,OpcaoEscolhida,AlunoId,AvaliacaoId")] Resposta resposta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(resposta).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AlunoId = new SelectList(db.Users, "Id", "Name", resposta.AlunoId);
+            ViewBag.AvaliacaoId = new SelectList(db.Avaliacoes, "Id", "Objetivo", resposta.AvaliacaoId);
+            ViewBag.QuestaoId = new SelectList(db.Questoes, "Id", "Texto", resposta.QuestaoId);
+            return View(resposta);
+        }
+
+        // GET: /Admin/Respostas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resposta resposta = db.Respostas.Find(id);
+            if (resposta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(resposta);
+        }
+
+        // POST: /Admin/Respostas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Resposta resposta = db.Respostas.Find(id);
+            db.Respostas.Remove(resposta);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ src/Avaliacoes.Web/Infrastructure/LikertNameConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Avaliacoes.Web.Infrastructure
+{
+    public static class LikertNameConverter
+    {
+        public static string Convert(int value)
+        {
+            switch (value)
+            {
+                case 0:
+                    return "Insatisfatório";
+                case 1:
+                    return "Pouco satisfatório";
+                case 2:
+                    return "Satisfatório";
+                case 3:
+                    return "Muito satisfatório";
+                case 4:
+                    return "Extremamente satisfatório";
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
